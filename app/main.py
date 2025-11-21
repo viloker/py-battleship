1b3aa10f00dda6a1b28c43a17b9522be74dd6439
viloker <yarmolykvadym@gmail.com>
2025-11-22 00:45:43 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,109 @@
+from typing import Any
+
+
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
+        self.is_alive = is_alive
+        self.row = row
+        self.column = column
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(self,
+                 start: tuple[int, int],
+                 end: tuple[int, int],
+                 is_drowned: bool = False) -> None:
+        self.is_drowned = is_drowned
+        if start[1] != end[1]:
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+            step = 1 if end[1] - start[1] >= 0 else - 1
+            self.decks = [Deck(start[0], x)
+                          for x in range(start[1], end[1] + step, step)]
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+        elif start[0] != end[0]:
+            step = 1 if end[0] - start[0] >= 0 else - 1
+            self.decks = [Deck(y, end[1])
+                          for y in range(start[0], end[0] + step, step)]
+
+        else:
+            self.decks = [Deck(start[0], start[1])]
+
+        self.coord = self.get_coord()
+
+    def get_deck(self, row: int, column: int) -> Deck | None:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return deck
+        return None
+
+    def fire(self, row: int, column: int) -> str:
+        deck = self.get_deck(row, column)
+        if deck is None:
+            return "Miss!"
+
+        if deck.is_alive is False:
+            return "Miss!"
+
+        deck.is_alive = False
+
+        if self.is_ship_alive():
+            return "Hit!"
+        else:
+            self.is_drowned = True
+            return "Sunk!"
+
+    def is_ship_alive(self) -> bool:
+        return any([deck.is_alive for deck in self.decks])
+
+    def get_coord(self) -> list[tuple[int, int]]:
+        return [(deck.row, deck.column) for deck in self.decks]
+
+    def __len__(self) -> int:
+        return len(self.coord)
+
+    def __repr__(self) -> str:
+        return f"{self.coord}"
+
+    def __contains__(self, item: Any) -> bool:
+        return item in self.coord
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(self, ships: list[tuple, tuple]) -> None:
+        self.field = [["~" for _ in range(10)] for _ in range(10)]
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+        self.ships = self.create_ships(ships)
+        if not self._validate_field():
+            raise ValueError("Your ships is not correct for the game")
+
+        self.draw_ships()
+
+    def create_ships(self, ships: list[tuple, tuple]) -> list[Ship]:
+
+        return [Ship(*ship) for ship in ships]
+
+    def _validate_field(self) -> bool:
+        length_ships = [len(ship) for ship in self.ships]
+        length_ships.sort()
+
+        if length_ships == [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]:
+            return True
+
+        return False
+
+    def draw_ship_on_field(self, ship: Ship) -> None:
+        ship_coords = ship.get_coord()
+
+        for coord in ship_coords:
+            self.field[coord[0]][coord[1]] = u"\u25A1"
+
+    def draw_ships(self) -> None:
+        for ship in self.ships:
+            self.draw_ship_on_field(ship)
+
+    def fire(self, location: tuple[int, int]) -> str:
+        for ship in self.ships:
+            if location in ship:
+                return ship.fire(location[0], location[1])
+
+        return "Miss!"
